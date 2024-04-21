705f89ff5e32019c1c383f17f475f4d7292c1a94
simonbotte <simon.botte@icloud.com>
2024-04-21 19:01:00 +0200

    Add favicon and meta tags for better user experience

--- app.vue
+++ app.vue
@@ -5,6 +5,42 @@ useHead({
             rel: "stylesheet",
             href: "https://use.typekit.net/job1kkm.css",
         },
+        {
+            rel: "apple-touch-icon",
+            sizes: "180x180",
+            href: "/apple-touch-icon.png",
+        },
+        {
+            rel: "icon",
+            type: "image/png",
+            sizes: "32x32",
+            href: "/favicon-32x32.png",
+        },
+        {
+            rel: "icon",
+            type: "image/png",
+            sizes: "16x16",
+            href: "/favicon-16x16.png",
+        },
+        {
+            rel: "manifest",
+            href: "/site.webmanifest",
+        },
+        {
+            rel: "mask-icon",
+            href: "/safari-pinned-tab.svg",
+            color: "#e1e634",
+        },
+    ],
+    meta: [
+        {
+            name: "msapplication-TileColor",
+            content: "#00aba9",
+        },
+        {
+            name: "theme-color",
+            content: "#ffffff",
+        },
     ],
 });
 </script>
